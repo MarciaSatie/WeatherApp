30482f2e0dfb43d43f054b3d71012ecc21552747
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2025-04-21 23:36:10 +0100

    Adding image depending if day or night

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,11 @@
 const weatherImg = {
   sun: "../assets/sun.png",
   cloudy: "../assets/cloudy.png",
-  rain: "../assets/rain.png"
+  rain: "../assets/rain.png",
+
+  night:"../assets/moon.png",
+  night_cloudy:"../assets/night_cloudy.png",
+  night_rain:"../assets/night_rain.png"
 };
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -70,8 +74,32 @@ dotify.utils.changeCity=(city)=>{
   dotify.utils.updateCardWind(cityData.daily.wind_speed_10m_max[0]);
   dotify.utils.updateSmallWeekCards(days, dayWeekNumber, cityData);
   img.src =dotify.utils.getImg(tempNow); 
-  
+  dotify.utils.wetherByTime(cityChoice);
 }
+
+dotify.utils.wetherByTime = (city) => {
+  const hourcards = document.getElementById("weatherByHour");
+  hourcards.innerHTML = ""; // clear previous content
+
+  for (let i = 0; i <= 6; i++) {
+    const cityHourly = dotify.utils.getHourObj(city);
+    const tempNow = cityHourly.hourly.temperature_2m[hour + i];
+    const weatherIMG = dotify.utils.getRandomImg();
+
+    const column = document.createElement("div");
+    column.className = "cell p-2"; // smaller padding
+
+    column.innerHTML = `
+      <div class="box has-background-light p-2 has-text-centered" style="min-height: 120px;">
+        <h1 class="title is-size-5 mb-2">${(hour + i) % 24} hr</h1>
+        <img src="${weatherIMG}" class="image" style="width: 60px; height: 60px;">
+        <p class="is-size-5 mt-1">Temp: ${tempNow}${celsius}</p>
+      </div>
+    `;
+
+    hourcards.appendChild(column);
+  }
+};
 
 
 
@@ -110,21 +138,38 @@ dotify.utils.updateSmallWeekCards = (days, dayWeekNumber, cityData) => {
   });
 };
 
+dotify.utils.imgByDayOrNight=()=>{
+  let weatherObj={};
+  if(hour>=7 && hour<=19){ // day time
+    weatherObj.rain = weatherImg.rain;
+    weatherObj.cloudy = weatherImg.cloudy;
+    weatherObj.clean = weatherImg.sun;
+  }else{
+    weatherObj.rain = weatherImg.night_rain;
+    weatherObj.cloudy = weatherImg.night_cloudy;
+    weatherObj.clean = weatherImg.night;
+  }
+
+  return weatherObj;
+}
+
 // Will return a imag of sun, rain or cloud
 dotify.utils.getImg = (tempNow) => {
-  return tempNow <=5? weatherImg.rain:
-         tempNow <=10?weatherImg.cloudy:weatherImg.sun;
+  const weatherObj =dotify.utils.imgByDayOrNight();
+  return tempNow <=5? weatherObj.rain:
+         tempNow <=10?weatherObj.cloudy:weatherObj.clean;
   
 };
 
 // Will return a imag of sun, rain or cloud by random
 dotify.utils.getRandomImg = () => {
+  const weatherObj =dotify.utils.imgByDayOrNight();
   min = 1;
   max = 3;
   let value = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  return value ==1? weatherImg.rain:
-  value ==2? weatherImg.cloudy:weatherImg.sun;
+  return value ==1? weatherObj.rain:
+  value ==2? weatherObj.cloudy:weatherObj.clean;
   
 };
 
